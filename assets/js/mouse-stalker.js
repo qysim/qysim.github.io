337ea162bb19936c3a3274391a5131b7cb38d105
qysim <qysim@naver.com>
2024-04-18 14:18:20 +0900

    fix: 마우스 스토커 변경

--- assets/js/mouse-stalker.js
+++ assets/js/mouse-stalker.js
@@ -1,194 +1,197 @@
-// @qysim: underwater 마우스 스토커 적용
+// @qysim: twinkle 마우스 스토커 적용
 // <![CDATA[
-  var colours=new Array("#d2bae5", "#b6c5e8", "#b6c5e8", "#d2bae5", "#ccc"); // colours for top, right, bottom and left borders and background of bubbles
-  var bubbles=66; // maximum number of bubbles on screen
-  var over_or_under="over"; // set to "over" for bubbles to always be on top, or "under" to allow them to float behind other objects
-  
-  /****************************
-  * JavaScript Bubble Cursor  *
-  *(c)2010-13 mf2fm web-design*
-  *  http://www.mf2fm.com/rv  *
-  * DON'T EDIT BELOW THIS BOX *
-  ****************************/
-  var x=ox=400;
-  var y=oy=300;
-  var swide=800;
-  var shigh=600;
-  var sleft=sdown=0;
-  var bubb=new Array();
-  var bubbx=new Array();
-  var bubby=new Array();
-  var bubbs=new Array();
-  var sploosh=false;
-  
-  function addLoadEvent(funky) {
-    var oldonload=window.onload;
-    if (typeof(oldonload)!='function') window.onload=funky;
-    else window.onload=function() {
-      if (oldonload) oldonload();
-      funky();
+var colour=new Array("#d2bae5", "#b6c5e8", "#ccc"); // in addition to "random" can be set to any valid colour eg "#f0f" or "red"
+var sparkles=50;
+
+/****************************
+*  Tinkerbell Magic Sparkle *
+*(c)2005-13 mf2fm web-design*
+*  http://www.mf2fm.com/rv  *
+* DON'T EDIT BELOW THIS BOX *
+****************************/
+var x=ox=400;
+var y=oy=300;
+var swide=800;
+var shigh=600;
+var sleft=sdown=0;
+var tiny=new Array();
+var star=new Array();
+var starv=new Array();
+var starx=new Array();
+var stary=new Array();
+var tinyx=new Array();
+var tinyy=new Array();
+var tinyv=new Array();
+
+window.onload=function() { if (document.getElementById) {
+  var i, rats, rlef, rdow;
+  for (var i=0; i<sparkles; i++) {
+    var rats=createDiv(3, 3);
+    rats.style.visibility="hidden";
+    rats.style.zIndex="999";
+    document.body.appendChild(tiny[i]=rats);
+    starv[i]=0;
+    tinyv[i]=0;
+    var rats=createDiv(5, 5);
+    rats.style.backgroundColor="transparent";
+    rats.style.visibility="hidden";
+    rats.style.zIndex="999";
+    var rlef=createDiv(1, 5);
+    var rdow=createDiv(5, 1);
+    rats.appendChild(rlef);
+    rats.appendChild(rdow);
+    rlef.style.top="2px";
+    rlef.style.left="0px";
+    rdow.style.top="0px";
+    rdow.style.left="2px";
+    document.body.appendChild(star[i]=rats);
+  }
+  set_width();
+  sparkle();
+}}
+
+function sparkle() {
+  var c;
+  if (Math.abs(x-ox)>1 || Math.abs(y-oy)>1) {
+    ox=x;
+    oy=y;
+    for (c=0; c<sparkles; c++) if (!starv[c]) {
+      star[c].style.left=(starx[c]=x)+"px";
+      star[c].style.top=(stary[c]=y+1)+"px";
+      star[c].style.clip="rect(0px, 5px, 5px, 0px)";
+      star[c].childNodes[0].style.backgroundColor=star[c].childNodes[1].style.backgroundColor=(colour=="random")?newColour():colour;
+      star[c].style.visibility="visible";
+      starv[c]=50;
+      break;
     }
   }
-  
-  addLoadEvent(buble);
-  
-  function buble() { if (document.getElementById) {
-    var i, rats, div;
-    for (i=0; i<bubbles; i++) {
-      rats=createDiv("3px", "3px");
-      rats.style.visibility="hidden";
-      rats.style.zIndex=(over_or_under=="over")?"1001":"0";
-  
-      div=createDiv("auto", "auto");
-      rats.appendChild(div);
-      div=div.style;
-      div.top="1px";
-      div.left="0px";
-      div.bottom="1px";
-      div.right="0px";
-      div.borderLeft="1px solid "+colours[3];
-      div.borderRight="1px solid "+colours[1];
-  
-      div=createDiv("auto", "auto");
-      rats.appendChild(div);
-      div=div.style;
-      div.top="0px";
-      div.left="1px";
-      div.right="1px";
-      div.bottom="0px"
-      div.borderTop="1px solid "+colours[0];
-      div.borderBottom="1px solid "+colours[2];
-  
-      div=createDiv("auto", "auto");
-      rats.appendChild(div);
-      div=div.style;
-      div.left="1px";
-      div.right="1px";
-      div.bottom="1px";
-      div.top="1px";
-      div.backgroundColor=colours[4];
-      if (navigator.appName=="Microsoft Internet Explorer") div.filter="alpha(opacity=50)";
-      else div.opacity=0.5;
-      document.body.appendChild(rats);
-      bubb[i]=rats.style;
+  for (c=0; c<sparkles; c++) {
+    if (starv[c]) update_star(c);
+    if (tinyv[c]) update_tiny(c);
+  }
+  setTimeout("sparkle()", 40);
+}
+
+function update_star(i) {
+  if (--starv[i]==25) star[i].style.clip="rect(1px, 4px, 4px, 1px)";
+  if (starv[i]) {
+    stary[i]+=1+Math.random()*3;
+    starx[i]+=(i%5-2)/5;
+    if (stary[i]<shigh+sdown) {
+      star[i].style.top=stary[i]+"px";
+      star[i].style.left=starx[i]+"px";
     }
+    else {
+      star[i].style.visibility="hidden";
+      starv[i]=0;
+      return;
+    }
+  }
+  else {
+    tinyv[i]=50;
+    tiny[i].style.top=(tinyy[i]=stary[i])+"px";
+    tiny[i].style.left=(tinyx[i]=starx[i])+"px";
+    tiny[i].style.width="2px";
+    tiny[i].style.height="2px";
+    tiny[i].style.backgroundColor=star[i].childNodes[0].style.backgroundColor;
+    star[i].style.visibility="hidden";
+    tiny[i].style.visibility="visible"
+  }
+}
+
+function update_tiny(i) {
+  if (--tinyv[i]==25) {
+    tiny[i].style.width="1px";
+    tiny[i].style.height="1px";
+  }
+  if (tinyv[i]) {
+    tinyy[i]+=1+Math.random()*3;
+    tinyx[i]+=(i%5-2)/5;
+    if (tinyy[i]<shigh+sdown) {
+      tiny[i].style.top=tinyy[i]+"px";
+      tiny[i].style.left=tinyx[i]+"px";
+    }
+    else {
+      tiny[i].style.visibility="hidden";
+      tinyv[i]=0;
+      return;
+    }
+  }
+  else tiny[i].style.visibility="hidden";
+}
+
+document.onmousemove=mouse;
+function mouse(e) {
+  if (e) {
+    y=e.pageY;
+    x=e.pageX;
+  }
+  else {
     set_scroll();
-    set_width();
-    bubble();
-  }}
-  
-  function bubble() {
-    var c;
-    if (Math.abs(x-ox)>1 || Math.abs(y-oy)>1) {
-      ox=x;
-      oy=y;
-      for (c=0; c<bubbles; c++) if (!bubby[c]) {
-        bubb[c].left=(bubbx[c]=x)+"px";
-        bubb[c].top=(bubby[c]=y-3)+"px";
-        bubb[c].width="3px";
-        bubb[c].height="3px"
-        bubb[c].visibility="visible";
-        bubbs[c]=3;
-        break;
-      }
-    }
-    for (c=0; c<bubbles; c++) if (bubby[c]) update_bubb(c);
-    setTimeout("bubble()", 40);
+    y=event.y+sdown;
+    x=event.x+sleft;
   }
-  
-  document.onmousedown=splash;
-  document.onmouseup=function(){clearTimeout(sploosh);};
-  
-  function splash() {
-    ox=-1;
-    oy=-1;
-    sploosh=setTimeout('splash()', 100);
+}
+
+window.onscroll=set_scroll;
+function set_scroll() {
+  if (typeof(self.pageYOffset)=='number') {
+    sdown=self.pageYOffset;
+    sleft=self.pageXOffset;
   }
-  
-  function update_bubb(i) {
-    if (bubby[i]) {
-      bubby[i]-=bubbs[i]/2+i%2;
-      bubbx[i]+=(i%5-2)/5;
-      if (bubby[i]>sdown && bubbx[i]>sleft && bubbx[i]<sleft+swide+bubbs[i]) {
-        if (Math.random()<bubbs[i]/shigh*2 && bubbs[i]++<8) {
-          bubb[i].width=bubbs[i]+"px";
-          bubb[i].height=bubbs[i]+"px";
-        }
-        bubb[i].top=bubby[i]+"px";
-        bubb[i].left=bubbx[i]+"px";
-      }
-      else {
-        bubb[i].visibility="hidden";
-        bubby[i]=0;
-        return;
-      }
-    }
+  else if (document.body && (document.body.scrollTop || document.body.scrollLeft)) {
+    sdown=document.body.scrollTop;
+    sleft=document.body.scrollLeft;
   }
-  
-  document.onmousemove=mouse;
-  function mouse(e) {
-    if (e) {
-      y=e.pageY;
-      x=e.pageX;
-    }
-    else {
-      set_scroll();
-      y=event.y+sdown;
-      x=event.x+sleft;
-    }
+  else if (document.documentElement && (document.documentElement.scrollTop || document.documentElement.scrollLeft)) {
+    sleft=document.documentElement.scrollLeft;
+    sdown=document.documentElement.scrollTop;
   }
-  
-  window.onresize=set_width;
-  function set_width() {
-    var sw_min=999999;
-    var sh_min=999999;
-    if (document.documentElement && document.documentElement.clientWidth) {
-      if (document.documentElement.clientWidth>0) sw_min=document.documentElement.clientWidth;
-      if (document.documentElement.clientHeight>0) sh_min=document.documentElement.clientHeight;
-    }
-    if (typeof(self.innerWidth)=='number' && self.innerWidth) {
-      if (self.innerWidth>0 && self.innerWidth<sw_min) sw_min=self.innerWidth;
-      if (self.innerHeight>0 && self.innerHeight<sh_min) sh_min=self.innerHeight;
-    }
-    if (document.body.clientWidth) {
-      if (document.body.clientWidth>0 && document.body.clientWidth<sw_min) sw_min=document.body.clientWidth;
-      if (document.body.clientHeight>0 && document.body.clientHeight<sh_min) sh_min=document.body.clientHeight;
-    }
-    if (sw_min==999999 || sh_min==999999) {
-      sw_min=800;
-      sh_min=600;
-    }
-    swide=sw_min;
-    shigh=sh_min;
+  else {
+    sdown=0;
+    sleft=0;
   }
-  
-  window.onscroll=set_scroll;
-  function set_scroll() {
-    if (typeof(self.pageYOffset)=='number') {
-      sdown=self.pageYOffset;
-      sleft=self.pageXOffset;
-    }
-    else if (document.body && (document.body.scrollTop || document.body.scrollLeft)) {
-      sdown=document.body.scrollTop;
-      sleft=document.body.scrollLeft;
-    }
-    else if (document.documentElement && (document.documentElement.scrollTop || document.documentElement.scrollLeft)) {
-      sleft=document.documentElement.scrollLeft;
-      sdown=document.documentElement.scrollTop;
-    }
-    else {
-      sdown=0;
-      sleft=0;
-    }
+}
+
+window.onresize=set_width;
+function set_width() {
+  var sw_min=999999;
+  var sh_min=999999;
+  if (document.documentElement && document.documentElement.clientWidth) {
+    if (document.documentElement.clientWidth>0) sw_min=document.documentElement.clientWidth;
+    if (document.documentElement.clientHeight>0) sh_min=document.documentElement.clientHeight;
   }
-  
-  function createDiv(height, width) {
-    var div=document.createElement("div");
-    div.style.position="absolute";
-    div.style.height=height;
-    div.style.width=width;
-    div.style.overflow="hidden";
-    div.style.backgroundColor="transparent";
-    return (div);
+  if (typeof(self.innerWidth)=='number' && self.innerWidth) {
+    if (self.innerWidth>0 && self.innerWidth<sw_min) sw_min=self.innerWidth;
+    if (self.innerHeight>0 && self.innerHeight<sh_min) sh_min=self.innerHeight;
   }
-  // ]]>
+  if (document.body.clientWidth) {
+    if (document.body.clientWidth>0 && document.body.clientWidth<sw_min) sw_min=document.body.clientWidth;
+    if (document.body.clientHeight>0 && document.body.clientHeight<sh_min) sh_min=document.body.clientHeight;
+  }
+  if (sw_min==999999 || sh_min==999999) {
+    sw_min=800;
+    sh_min=600;
+  }
+  swide=sw_min;
+  shigh=sh_min;
+}
+
+function createDiv(height, width) {
+  var div=document.createElement("div");
+  div.style.position="absolute";
+  div.style.height=height+"px";
+  div.style.width=width+"px";
+  div.style.overflow="hidden";
+  return (div);
+}
+
+function newColour() {
+  var c=new Array();
+  c[0]=255;
+  c[1]=Math.floor(Math.random()*256);
+  c[2]=Math.floor(Math.random()*(256-c[1]/2));
+  c.sort(function(){return (0.5 - Math.random());});
+  return ("rgb("+c[0]+", "+c[1]+", "+c[2]+")");
+}
+// ]]>
